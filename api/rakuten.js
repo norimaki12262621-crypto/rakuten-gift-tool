e21dc915dbad5f5d8716b5e4eac4d55a9c616dfe
norimaki12262621-crypto <norimaki12262621@gmail.com>
2026-05-04 00:29:14 +0900

    Update rakuten.js

--- api/rakuten.js
+++ api/rakuten.js
@@ -2,7 +2,19 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
 
-  const params = new URLSearchParams(req.query);
+  const { keyword, minPrice, maxPrice, sort, hits } = req.query;
+  
+  const params = new URLSearchParams({
+    keyword: keyword || '母の日 プレゼント',
+    hits: hits || 30,
+    minPrice: minPrice || 3000,
+    maxPrice: maxPrice || 7000,
+    sort: sort || '-reviewCount',
+    formatVersion: 2,
+    imageFlag: 1,
+    applicationId: '9a9bb16b-a393-414a-ad63-ea58ecf01daa',
+  });
+
   const url = `https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601?${params}`;
 
   try {
